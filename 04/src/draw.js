682c87ff0017f9ee2aaa084cff397ed0050a1171
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2022-11-19 22:56:28 -0300

    Implement example 04

---
+++ 04/src/draw.js
@@ -0,0 +1,32 @@
+import { setLineWidth } from './utils.js';
+
+export function drawPoint(ctx, { x, y }) {
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI * 2, true);
+  ctx.closePath();
+  ctx.fill();
+}
+
+export function drawLine(ctx, coordinates) {
+  setLineWidth(ctx, 4);
+  ctx.beginPath();
+  coordinates.forEach(({ x, y }, index) => {
+    if (index === 0) ctx.moveTo(x, y);
+    ctx.lineTo(x, y);
+  });
+  ctx.closePath();
+  ctx.stroke();
+}
+
+export function drawPolygon(ctx, coordinates = []) {
+  setLineWidth(ctx, 4);
+  coordinates.forEach((pCoordinates) => {
+    ctx.beginPath();
+    pCoordinates.forEach(({ x, y }, index) => {
+      if (index === 0) ctx.moveTo(x, y);
+      ctx.lineTo(x, y);
+    });
+    ctx.closePath();
+    ctx.stroke();
+  });
+}
